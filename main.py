dc0bdf8138672204c6fda9d44122cde68dabcd60
Milo <mgill00027@gmail.com>
2025-03-04 10:30:42 +1000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,33 @@
+mySprite = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    SpriteKind.player)
+
+def on_forever():
+    while controller.up:
+        pass
+    
+    while controller.down:
+        pass
+
+    while controller.left:
+        pass
+
+    while controller.right:
+        pass
+forever(on_forever)
