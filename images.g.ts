260eb99b288d444ee4abb527989fe92e6744620b
Milo <mgill00027@gmail.com>
2025-03-14 10:19:03 +1000

    Added climber sprite set vy to 0

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Climber back":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f 6 6 6 6 f f . . . . 
+. . . f 6 6 6 6 6 6 6 6 f . . . 
+. . f f 6 6 6 6 6 6 6 6 f f . . 
+. . f f 6 6 6 6 6 6 6 6 f f . . 
+. . f 6 6 6 6 6 6 6 6 6 6 f . . 
+. . f 6 6 6 6 6 6 6 6 6 6 f . . 
+. . f 6 6 6 6 6 6 6 6 6 6 f . . 
+. . f 6 6 6 6 6 6 6 6 6 6 f . . 
+. . f 6 6 6 6 6 6 6 6 6 6 f . . 
+. . . f 6 6 6 6 6 6 6 6 f . . . 
+. . 8 6 f f f f f f f f 6 8 . . 
+. . 6 d f 8 8 8 8 8 8 f d 6 . . 
+. . 6 6 f 6 6 6 6 6 6 f 6 6 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
