95de0bf7a6d937df308372088fdd0e33689f2517
Milo <mgill00027@gmail.com>
2025-03-18 10:27:31 +1000

    Added cooked walking animation 🥀

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
+function Destroy_and_subtract () {
+    sprites.destroy(Snowball)
+    Snowballcount += -1
+}
+let Snowball: Sprite = null
 scene.setBackgroundImage(assets.image`Bigger mountain`)
 let Climber = sprites.create(assets.image`Climber back`, SpriteKind.Player)
 controller.moveSprite(Climber, 100, 0)
 Climber.y = 100
 Climber.setStayInScreen(true)
-let Snowball = sprites.create(assets.image`Snowball`, SpriteKind.Enemy)
-Snowball.x = scene.screenWidth() / 2
+Snowball = sprites.create(assets.image`Snowball`, SpriteKind.Enemy)
+let Snowballcount = 1
+Snowball.x = randint(0, scene.screenWidth())
 Snowball.y = 0
+forever(function () {
+    if (Snowballcount < 5) {
+    	
+    }
+})
+game.onUpdateInterval(500, function () {
+    if (Snowball.y > scene.screenHeight()) {
+        Destroy_and_subtract()
+        Snowball = sprites.create(assets.image`Snowball`, SpriteKind.Enemy)
+        Snowball.x = randint(0, scene.screenWidth())
+        Snowball.y = 0
+    }
+})
