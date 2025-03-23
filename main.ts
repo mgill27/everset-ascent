88ef3b78896198762372072f5f5f1ed4edffada2
Milo <mgill00027@gmail.com>
2025-03-24 09:32:43 +1000

    :)

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+function SpawnSnowballs () {
+    for (let index = 0; index < randint(0, 3); index++) {
+        if (activeSnowballs < 3) {
+            Snowball = sprites.create(assets.image`Snowball`, SpriteKind.Projectile)
+            Snowball.x = randint(0, scene.screenWidth())
+            Snowball.y = 0
+            Snowball.setVelocity(0, randint(0, speed))
+            activeSnowballs += 1
+        }
+    }
+}
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     if (flashing == true) {
     	
@@ -65,19 +76,34 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     flashing = true
     Flash()
     flashing = false
+    pause(500)
 })
 let flashing = false
+let Snowball: Sprite = null
 let Climber: Sprite = null
-let speed = 50
+let speed = 0
+let activeSnowballs = 0
+game.splash("You need to reach 100 altitude-")
+game.splash("before the mountain collapses.")
+scene.setBackgroundImage(assets.image`Smile`)
+game.splash("Good luck!")
+info.setScore(0)
+activeSnowballs = 0
+speed = 50
 scene.setBackgroundImage(assets.image`Bigger mountain`)
 Climber = sprites.create(assets.image`Climber back`, SpriteKind.Player)
 controller.moveSprite(Climber, 100, 0)
 Climber.y = 100
 Climber.setStayInScreen(true)
-let Snowball = sprites.create(assets.image`Snowball`, SpriteKind.Projectile)
-Snowball.x = randint(0, scene.screenWidth())
+Snowball = sprites.create(assets.image`Snowball`, SpriteKind.Projectile)
+Snowball.x = randint(0.5, scene.screenWidth() - 0.5)
 Snowball.y = 0
 Snowball.setVelocity(0, speed)
+game.onUpdateInterval(5000, function () {
+    if (info.score() > 5) {
+        SpawnSnowballs()
+    }
+})
 game.onUpdateInterval(500, function () {
     if (Snowball.y > scene.screenHeight()) {
         Snowball = sprites.create(assets.image`Snowball`, SpriteKind.Enemy)
